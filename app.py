e74e658ed64e71514c11cb92ea5064721ce0ca3e
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2024-02-03 20:39:58 -0800

    testing

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-import connexion
+import connexion, datetime, json, yaml, logging, logging.config
 from connexion import NoContent
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from base import Base
-import datetime
-import json
 from update_event_data import update_event_data
 from media_upload import MediaUpload
 from media_playback import MediaPlayback
 
+with open('log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+    logging.config.dictConfig(log_config)
+    
+logger = logging.getLogger('basicLogger')
+logger.setLevel(logging.DEBUG)
 
-#Receiver 
-DB_ENGINE = create_engine('sqlite:///media_server.db')  # Connect to your SQLite database
+#Receiver DB Setup for credentials (Load info from app_conf.yml, add as dict, access values)
+with open('app_conf.yml', 'r') as config_file:
+    app_config = yaml.safe_load(config_file)
+db_config = app_config['datastore']
+db_user = db_config['user']
+db_password = db_config['password']
+db_hostname = db_config['hostname']
+db_port = db_config.get('port', 3306)  # Provide a default port if not specified
+db_name = db_config['db']
+
+DB_ENGINE = create_engine(f'mysql+pymysql://{db_user}:{db_password}@{db_hostname}:{db_port}/{db_name}')
+
+# DB_ENGINE = create_engine('sqlite:///media_server.db')  # Connect to your SQLite database
 Base.metadata.bind = DB_ENGINE
 DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
@@ -32,8 +47,9 @@ def media_upload(body):
         "mediaId": str(new_upload.id)
     }
     session.close()
+    logger.info(f'Stored event "media_upload" request with a trace id of {body["trace_id"]}')
     #Cant return NoContent due to strict validation 
-    return response, 201
+    return response.json(), response.status_code
 
 def media_playback(body):
     session = DB_SESSION()
@@ -52,7 +68,8 @@ def media_playback(body):
         "userID":str(new_playback.userID)
     }
     session.close()
-    return NoContent, 201
+    logger.info(f'Stored event "media_playback" request with a trace id of {body["trace_id"]}')
+    return response.json(), response.status_code
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
--- create_tables_mysql.py
+++ create_tables_mysql.py
@@ -1,1 +1,45 @@
 import mysql.connector
+# create_database.py
+db_conn = mysql.connector.connect(host="localhost", user="ram", password="Password", database="arch")
+db_cursor = db_conn.cursor()
+
+# Base = declarative_base()
+db_cursor.execute('''
+    CREATE TABLE media_uploads(
+        id INT NOT NULL AUTO_INCREMENT,
+        fileSize INT NOT NULL,
+        mediaType ENUM('Photo', 'Video') NOT NULL,
+        uploadTimestamp VARCHAR(100) NOT NULL,
+        userID BINARY(16) NOT NULL,
+        date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        trace_id BINARY(16) NOT NULL,
+        CONSTRAINT media_uploads_pk PRIMARY KEY (id)
+    )
+''')
+
+db_cursor.execute('''
+    CREATE TABLE media_playbacks(
+        id INT NOT NULL AUTO_INCREMENT,
+        mediaId VARCHAR(255) NOT NULL,
+        playbackStartTime DATETIME NOT NULL,
+        userID BINARY(16) NOT NULL ,
+        playbackId INT NOT NULL,
+        playbackDuration INT,
+        date_created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        trace_id BINARY(16) NOT NULL,
+        CONSTRAINT media_playbacks_pk PRIMARY KEY (id)
+    )
+''')
+
+# class MediaUpload(Base):
+#     __tablename__ = 'media_uploads'
+#     id = Column(Integer, primary_key=True)
+#     fileSize = Column(Integer)
+#     mediaType = Column(String)
+#     uploadTimestamp = Column(DateTime)
+#     userID = Column(Uuid)
+#     date_created = Column(DateTime, default=datetime.datetime.utcnow)
+#     trace_id = Column(UUIDType, nullable=False)
+
+db_conn.commit()
+db_conn.close()
